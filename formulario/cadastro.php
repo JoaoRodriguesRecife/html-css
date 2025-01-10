15c918ed1d3c7e5497cbcbe19fb7a2b92e97beb5
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-01-10 11:37:25 -0300

    atualizaçao

--- formulario/cadastro.php
+++ formulario/cadastro.php
@@ -3,11 +3,11 @@ codigo
 <form action="cadastro.php" method="post" autocomplete="on">
     <p>
         <label for="iusuario">Usuario:</label>
-        <input type="text" name="usuario" id="iusuario" required minlength="5" maxlength="10" placeholder="usuario">
+        <input type="text" name="usuario" id="iusuario" required minlength="5" maxlength="10" placeholder="usuario" autocomplete="username">
     </p>
     <p>
         <label for="isenha">Senha:</label>
-        <input type="password" name="Senha" id="isenha" required minlength="5" maxlength="10" placeholder="senha">
+        <input type="password" name="Senha" id="isenha" required minlength="5" maxlength="10" placeholder="senha" autocomplete="current-password">
     </p>
     <p>
         <input type="submit" value="Enviar">
