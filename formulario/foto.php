2329c5538747c8fd601393f82692fc4a3ec63c00
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-01-14 11:26:46 -0300

    add arqu

---
+++ formulario/foto.php
@@ -0,0 +1,20 @@
+<form action="#" method="post">
+        <fieldset>
+            <legend>cor</legend>
+            <label for="icor">Cor:</label>
+            <input type="color" name="cor" id="icor">
+    </fieldset>
+    <fieldset>
+        <legend>satisfaçao</legend>
+        <label for="isatis">Satisfaçao:</label>
+        <input type="range" name="satis" id="isatis" value="1">
+    </fieldset>
+    <fieldset>
+        <legend>Foto Perfil</legend>
+        <label for="ifoto">escolha a foto</label>
+        <input type="file" name="foto" id="ifoto">
+    </fieldset>
+    <p>
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Limpar">
+    </p>
