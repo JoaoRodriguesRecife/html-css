d7de1599e5efad8392ca5e7f6bcc9330aaab07c4
Joao Rodrigues <110858961+JoaoRodriguesRecife@users.noreply.github.com>
2025-01-14 10:42:01 -0300

    add esportes

---
+++ formulario/codigo.php
@@ -0,0 +1,17 @@
+<form action="#" method="post">
+    <fieldset>
+        <legend>Sexo</legend>
+        <input type="radio" name="sexo" id="ifemi"><label for="ifemi">Feminino</label><br>
+        <input type="radio" name="sexo" id="imas"><label for="imas">Masculino</label>
+    </fieldset>
+    <fieldset>
+        <legend>Esportes</legend>
+        <input type="checkbox" name="cilismo" id="iciclismo"><label for="iciclismo">Ciclismo</label><br>
+        <input type="checkbox" name="futebol" id="ifutebol"><label for="ifutebol">Futebol</label><br>
+        <input type="checkbox" name="volei" id="ivolei"><label for="ivolei">Volei</label>
+    </fieldset>
+    <p>
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Limpa">
+    </p>
+</form>
